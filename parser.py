5e9950d2b14400343c18d22b0f0ddaa7d19f6f10
lari4 <iam@lari4.com>
2019-12-04 15:21:23 +0300

    add rate limit decorator for parse func

--- parser.py
+++ parser.py
@@ -1,5 +1,39 @@
 import argparse
 import datetime
+import time
+import threading
+
+from functools import wraps
+
+
+def rate_limited(max_per_second):
+    """
+    Decorator that make functions not be called faster than
+    """
+    lock = threading.Lock()
+    min_interval = 1.0 / float(max_per_second)
+
+    def decorate(func):
+        last_time_called = [0.0]
+
+        @wraps(func)
+        def rate_limited_function(*args, **kwargs):
+            lock.acquire()
+            elapsed = time.perf_counter() - last_time_called[0]
+            left_to_wait = min_interval - elapsed
+
+            if left_to_wait > 0:
+                time.sleep(left_to_wait)
+
+            lock.release()
+
+            ret = func(*args, **kwargs)
+            last_time_called[0] = time.perf_counter()
+            return ret
+
+        return rate_limited_function
+
+    return decorate
 
 
 def valid_date(s):
@@ -10,9 +44,9 @@ def valid_date(s):
         raise argparse.ArgumentTypeError(msg)
 
 
+@rate_limited(1)
 def parse(security, date):
     base_url = "https://www.moex.com/api/contract/OpenOptionService/"
-
 
 
 def main(security):
