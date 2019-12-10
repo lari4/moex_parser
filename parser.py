c8bab4eadd383180a7c0288c155421a6c5c042d9
Alexey Larionov <iam@lari4.com>
2019-12-10 16:17:48 +0300

    style worksheet

--- parser.py
+++ parser.py
@@ -105,26 +105,110 @@ def save_to_excel(path, data):
     logger.info(f"Сохранение данных в файл {path}")
     try:
         workbook = xlsxwriter.Workbook(path)
-        worksheet = workbook.add_worksheet()
 
-        for line, date_data in enumerate(data[::-1]):
+        title_right_border_bold = workbook.add_format()
+        title_right_border_bold.set_align('center')
+        title_right_border_bold.set_align('vcenter')
+        title_right_border_bold.set_bold(True)
+        title_right_border_bold.set_text_wrap(True)
+        title_right_border_bold.set_right(5)
+
+        title_bottom_border_bold = workbook.add_format()
+        title_bottom_border_bold.set_align('center')
+        title_bottom_border_bold.set_align('vcenter')
+        title_bottom_border_bold.set_bold(True)
+        title_bottom_border_bold.set_text_wrap(True)
+        title_bottom_border_bold.set_bottom(5)
+
+        title_right_bottom_border_bold = workbook.add_format()
+        title_right_bottom_border_bold.set_align('center')
+        title_right_bottom_border_bold.set_align('vcenter')
+        title_right_bottom_border_bold.set_bold(True)
+        title_right_bottom_border_bold.set_text_wrap(True)
+        title_right_bottom_border_bold.set_right(5)
+        title_right_bottom_border_bold.set_bottom(5)
+
+        title_format = workbook.add_format()
+        title_format.set_align('center')
+        title_format.set_align('vcenter')
+        title_format.set_bold(True)
+        title_format.set_text_wrap(True)
+
+        right_bold_border = workbook.add_format()
+        right_bold_border.set_right(5)
+
+        right_bottom_bold_border = workbook.add_format()
+        right_bottom_bold_border.set_right(5)
+        right_bottom_bold_border.set_bottom(5)
+
+        bottom_bold_border = workbook.add_format()
+        bottom_bold_border.set_bottom(5)
+
+        worksheet = workbook.add_worksheet()
+        worksheet.write(0, 0, '', right_bold_border)
+        worksheet.write(1, 0, '', right_bold_border)
+        worksheet.write(1, 1, '', right_bold_border)
+        worksheet.set_column(0, 1, 12)
+        worksheet.set_column(2, 20, 8)
+
+        worksheet.merge_range(0, 1, 0, 6, "Открытые позиции", cell_format=title_right_border_bold)
+        worksheet.merge_range(0, 7, 0, 11, "Изменение", cell_format=title_right_border_bold)
+        worksheet.merge_range(0, 12, 0, 16, "Относительное изменение в %", cell_format=title_right_border_bold)
+        worksheet.merge_range(0, 17, 0, 21, "Количество лиц", cell_format=title_right_border_bold)
+
+        worksheet.merge_range(1, 2, 1, 3, "Физ лица", cell_format=title_format)
+        worksheet.merge_range(1, 4, 1, 5, "Ю лица", cell_format=title_format)
+        worksheet.write(1, 6, '', right_bold_border)
+        worksheet.merge_range(1, 7, 1, 8, "Физ лица", cell_format=title_format)
+        worksheet.merge_range(1, 9, 1, 10, "Ю лица", cell_format=title_format)
+        worksheet.write(1, 11, '', right_bold_border)
+        worksheet.merge_range(1, 12, 1, 13, "Физ лица", cell_format=title_format)
+        worksheet.merge_range(1, 14, 1, 15, "Ю лица", cell_format=title_format)
+        worksheet.write(1, 16, '', right_bold_border)
+        worksheet.merge_range(1, 17, 1, 18, "Физ лица", cell_format=title_format)
+        worksheet.merge_range(1, 19, 1, 20, "Ю лица", cell_format=title_format)
+        worksheet.write(1, 21, '', right_bold_border)
+        worksheet.set_row(2, 35, cell_format=title_format)
+        worksheet.write(2, 0, 'Запрошенная дата', title_right_bottom_border_bold)
+        worksheet.write(2, 1, 'Предоставленная дата', title_right_bottom_border_bold)
+
+        column = 2
+        for _ in range(4):
+            worksheet.write(2, column, 'Длинные', title_bottom_border_bold)
+            column += 1
+            worksheet.write(2, column, 'Короткие', title_bottom_border_bold)
+            column += 1
+            worksheet.write(2, column, 'Длинные', title_bottom_border_bold)
+            column += 1
+            worksheet.write(2, column, 'Короткие', title_bottom_border_bold)
+            column += 1
+            worksheet.write(2, column, 'Итого', title_right_bottom_border_bold)
+            column += 1
+        for line, date_data in enumerate(data[::-1], start=3):
+            if line == len(data) + 2:
+                style = right_bottom_bold_border
+                bottom_style = bottom_bold_border
+            else:
+                style = right_bold_border
+                bottom_style = None
             column = 0
             requested_date = date_data[0]
-            worksheet.write(line, column, requested_date)
+
+            worksheet.write(line, column, requested_date, style)
             column += 1
             for _ in date_data[1]:
                 if column == 1:
-                    worksheet.write(line, column, _['Date'])
+                    worksheet.write(line, column, _['Date'], style)
                     column += 1
-                worksheet.write_number(line, column, float(parse_number(_['PhysicalLong'])))
+                worksheet.write_number(line, column, float(parse_number(_['PhysicalLong'])), bottom_style)
                 column += 1
-                worksheet.write_number(line, column, float(parse_number(_['PhysicalShort'])))
+                worksheet.write_number(line, column, float(parse_number(_['PhysicalShort'])), bottom_style)
                 column += 1
-                worksheet.write_number(line, column, float(parse_number(_['JuridicalLong'])))
+                worksheet.write_number(line, column, float(parse_number(_['JuridicalLong'])), bottom_style)
                 column += 1
-                worksheet.write_number(line, column, float(parse_number(_['JuridicalShort'])))
+                worksheet.write_number(line, column, float(parse_number(_['JuridicalShort'])), bottom_style)
                 column += 1
-                worksheet.write_number(line, column, float(parse_number(_['Summary'])))
+                worksheet.write_number(line, column, float(parse_number(_['Summary'])), style)
                 column += 1
         workbook.close()
         return True
@@ -134,8 +218,15 @@ def save_to_excel(path, data):
 
 
 def main(security, date_from, date_to, path):
-    logger.info(f"Запуск скачивания с параметрами инструмент={security} с={date_from} "
-                f"по={date_to} директория={path}")
+    if not date_to:
+        file_name = f'Данные по {security} за {date_from.strftime("%d.%m.%Y")}.xlsx'
+        date_to = date_from
+    else:
+        file_name = f'Данные по {security} за {date_from.strftime("%d.%m.%Y")}' \
+                    f'-{date_to.strftime("%d.%m.%Y")}.xlsx'
+    logger.info(f"Запуск скачивания с параметрами инструмент={security}"
+                f" с={date_from.strftime('%d.%m.%Y')}"
+                f" по={date_to.strftime('%d.%m.%Y')} директория={path}")
     if not test_internet_connection():
         logger.error(f"Нет подключения к интернету!")
         sys.exit(0)
@@ -148,8 +239,6 @@ def main(security, date_from, date_to, path):
     if not save_dir.exists() and not save_dir.is_dir():
         logger.error(f"Папка {path} не существует!")
         sys.exit(0)
-    if not date_to:
-        date_to = date_from
     date_delta = date_to - date_from
     data = []
     for date in range(date_delta.days + 1):
@@ -161,16 +250,14 @@ def main(security, date_from, date_to, path):
             logger.info(f"Для {security} нет данных за {day.strftime('%d.%m.%Y')}")
     if data:
         if not save_to_excel(
-            path=save_dir / f'{security}_{date_from.strftime("%d.%m.%Y")}'
-                            f'_{date_to.strftime("%d.%m.%Y")}.xlsx',
+            path=save_dir / file_name,
             data=data,
         ):
             attempts = 0
             while attempts < 5:
                 new_path = Path(input("Введите новую директорию для сохранения: "))
                 if save_to_excel(
-                    path=new_path / f'{security}_{date_from.strftime("%d.%m.%Y")}'
-                                    f'_{date_to.strftime("%d.%m.%Y")}.xlsx',
+                    path=new_path / file_name,
                     data=data,
                 ):
                     break
